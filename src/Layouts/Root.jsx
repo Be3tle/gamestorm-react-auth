bfdf8dc8946b76e1db1bda4ee5e83ebdc77b79f9
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-22 02:29:23 +0600

    added footer to root layout

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Header/Navbar';
+import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
   return (
     <div className="font-display">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
