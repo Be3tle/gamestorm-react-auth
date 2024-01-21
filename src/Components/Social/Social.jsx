bc3925068cefbf12f4d4d84db82cc07072c0ce1a
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-22 02:13:54 +0600

    social component size reduced in mobile res

--- src/Components/Social/Social.jsx
+++ src/Components/Social/Social.jsx
@@ -1,23 +1,26 @@
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Social = () => {
   return (
-    <div>
+    <div className="">
       <div className="p-2 mb-6 text-white">
         <h2 className="text-xl mb-4">Find Us On</h2>
         <a
-          className="p-2 flex text-lg items-center border rounded-t-lg"
+          className="p-2 flex sm:text-xs lg:text-lg items-center border rounded-t-lg"
           href=""
         >
           <FaFacebook className="mr-3"></FaFacebook>
           <span>Facebook</span>
         </a>
-        <a className="p-2 flex text-lg items-center border-x" href="">
+        <a
+          className="p-2 flex sm:text-xs lg:text-lg items-center border-x"
+          href=""
+        >
           <FaTwitter className="mr-2"></FaTwitter>
           <span>Twitter</span>
         </a>
         <a
-          className="p-2 flex text-lg items-center border rounded-b-lg"
+          className="p-2 flex sm:text-xs lg:text-lg items-center border rounded-b-lg"
           href=""
         >
           <FaInstagram className="mr-2"></FaInstagram>
