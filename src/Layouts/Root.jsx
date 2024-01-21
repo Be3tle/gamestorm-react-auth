f5d44f16f838b2a192813fd05b31d1be3c1b6ac1
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-21 23:45:20 +0600

    layout fixed width removed

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div className="max-w-6xl mx-auto font-display">
+    <div className="font-display">
       <Outlet></Outlet>
     </div>
   );
